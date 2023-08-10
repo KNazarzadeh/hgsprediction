74146ca5c84493ca048245f1dda8fb15c06785fc
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2023-08-10 17:39:31 +0200

    Modified plots.

--- exec/brain_correlates/stroke_mri_brain_correlates.py
+++ exec/brain_correlates/stroke_mri_brain_correlates.py
@@ -56,85 +56,56 @@ male_best_model_trained = load_best_model_trained(
 print(male_best_model_trained)
 ##############################################################################
 stroke_all_columns, df_female, df_male, X, y = prepare_stroke(target)
-print("===== Done! =====")
-embed(globals(), locals())
 ##############################################################################
-f_days = df_female[df_female['31-0.0']==0.0]['post_days']
-m_days = df_male[df_male['31-0.0']==1.0]['post_days']
-
+df_female["days"] = df_female[df_female['31-0.0']==0.0]['post_days']
+df_male["days"] = df_male[df_male['31-0.0']==1.0]['post_days']
 ##############################################################################
 #female
 y_true = df_female[y]
 y_pred = female_best_model_trained.predict(df_female[X])
-df_female["actual_hgs"] = y_true
-df_female["predicted_hgs"] = y_pred
+df_female["hgs_actual"] = y_true
+df_female["hgs_predicted"] = y_pred
 df_female["hgs_diff"] = y_true - y_pred
 # corr_female_diff, p_female_diff = spearmanr(df_female["hgs_diff"], f_days/365)
 
 #male
 y_true = df_male[y]
 y_pred = male_best_model_trained.predict(df_male[X])
-df_male["actual_hgs"] = y_true
-df_male["predicted_hgs"] = y_pred
+df_male["hgs_actual"] = y_true
+df_male["hgs_predicted"] = y_pred
 df_male["hgs_diff"] = y_true - y_pred
-# corr_male, p_male = spearmanr(df_male["hgs_diff"], m_days/365)
-# print("===== Done! =====")
-# embed(globals(), locals())
+
 df_female_output = pd.concat([df_female[X], df_female[y]], axis=1)
-df_female_output = pd.concat([df_female_output, df_female[['predicted_hgs', 'hgs_diff']]], axis=1)
+df_female_output = pd.concat([df_female_output, df_female[['hgs_predicted', 'hgs_diff']]], axis=1)
 
 df_male_output = pd.concat([df_male[X], df_male[y]], axis=1)
-df_male_output = pd.concat([df_male_output, df_male[['predicted_hgs', 'hgs_diff']]], axis=1)
+df_male_output = pd.concat([df_male_output, df_male[['hgs_predicted', 'hgs_diff']]], axis=1)
+
 print(df_female_output)
+print(df_male_output)
+
+df_both_gender = pd.concat([df_female, df_male], axis=0)
+print(df_both_gender)
+# print("===== Done! =====")
+# embed(globals(), locals())
 ###############################################################################################################################################################
+# create_regplot(target,
+#             x_male=m_days/365,
+#             y_male=df_male["predicted_hgs"],
+#             x_female=f_days/365,
+#             y_female=df_female["predicted_hgs"],
+#             corr_male=spearmanr(df_male["predicted_hgs"], m_days/365)[0],
+#             corr_female=spearmanr(df_female["predicted_hgs"], f_days/365)[0],
+#             title="Predicted",
+#             y_label="Predicted HGS",)
 create_regplot(target,
-            x_male=m_days/365,
-            y_male=df_male["predicted_hgs"],
-            x_female=f_days/365,
-            y_female=df_female["predicted_hgs"],
-            corr_male=spearmanr(df_male["predicted_hgs"], m_days/365)[0],
-            corr_female=spearmanr(df_female["predicted_hgs"], f_days/365)[0],
-            title="Predicted",
-            y_label="Predicted HGS",)
+                df=df_both_gender,
+                x="years",
+                y="diff",
+                corr=spearmanr(df_both_gender["hgs_diff"], df_both_gender["days"]/365)[0],
+                title="Actual-Predicted",
+                y_label="Actual-Predicted",
+)
 print("===== Done! =====")
 embed(globals(), locals())
 ###############################################################################################################################################################
-# Create the actual HGS vs predicted HGS plot for females and fefemales separately
-fig = plt.figure(figsize=(15,15))
-sns.set_context("poster")
-ax = fig.add_subplot(111)
-plt.tick_params(axis='both', labelsize=30)
-sns.regplot(x=m_days/365, y=df_male["hgs_diff"], line_kws={"color": "grey"}, scatter_kws={"color": "blue"})
-sns.regplot(x=f_days/365, y=df_female["hgs_diff"], scatter_kws={"color": "red"}, line_kws={"color": "grey"})
-
-ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-# Add two text annotations to the corner
-text_female = 'r= ' + str(format(spearmanr(df_female["predicted_hgs"], f_days)[0], '.3f'))
-text_male = 'r= ' + str(format(spearmanr(df_male["predicted_hgs"], m_days)[0], '.3f'))
-
-xmax = np.max(ax.get_xlim())
-ymax = np.max(ax.get_ylim())
-xmin = np.min(ax.get_xlim())
-ymin = np.min(ax.get_ylim())
-
-plt.text(xmax - 0.15 * xmax, ymax - 0.005 * ymax,text_female, verticalalignment='top',
-         horizontalalignment='right', fontsize=24, fontweight="bold", color="red")
-
-plt.text(xmax - 0.15 * xmax, ymax - 0.07 * ymax, text_male, verticalalignment='top',
-         horizontalalignment='right', fontsize=24, fontweight="bold", color="blue")
-
-plt.xlabel('Post-stroke years', fontsize=30, fontweight="bold")
-plt.ylabel('Predicted HGS', fontsize=30, fontweight="bold")
-plt.title("Difference (Actual-Predicted)HGS vs Post-stroke years", fontsize=30, fontweight="bold", y=1.05)
-plt.tight_layout()  # Adjust layout to prevent clipping of legend
-
-ax_lim_min = np.min(plt.xlim())
-ax_lim_max = np.max(plt.xlim())
-
-plt.plot(.5,.5, 'ko')
-
-plt.plot([xmin+1, xmax- 1], [ymin, ymax], 'k--')
-
-plt.show()
-plt.savefig(f"090823correlate_diff_actual_predicted_hgs_storke_mri_both_gender_post_diff_years.png")
-plt.close()
--- hgsprediction/plots/make_plot.py
+++ hgsprediction/plots/make_plot.py
@@ -4,42 +4,95 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# def create_regplot(target,
+#                    x_male,
+#                    y_male,
+#                    x_female,
+#                    y_female,
+#                    corr_male,
+#                    corr_female,
+#                    title,
+#                    y_label,
+# ):
+
+#     fig = plt.figure(figsize=(15,15))
+#     sns.set_context("poster")
+#     ax = fig.add_subplot(111)
+#     plt.tick_params(axis='both', labelsize=30)
+#     sns.regplot(x=x_male, y=y_male, line_kws={"color": "grey"}, scatter_kws={"color": "blue"})
+#     sns.regplot(x=x_female, y=y_female, scatter_kws={"color": "red"}, line_kws={"color": "grey"})
+
+#     ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+
+#     # Add two text annotations to the corner
+#     text_male = 'r= ' + str(format(corr_male, '.3f'))
+#     text_female = 'r= ' + str(format(corr_female, '.3f'))
+
+#     xmax = np.max(ax.get_xlim())
+#     ymax = np.max(ax.get_ylim())
+#     xmin = np.min(ax.get_xlim())
+#     ymin = np.min(ax.get_ylim())
+    
+#     plt.text(xmax - 0.15 * xmax, ymax - 0.005 * ymax, text_male, verticalalignment='top',
+#             horizontalalignment='right', fontsize=24, fontweight="bold", color="blue")
+#     plt.text(xmax - 0.15 * xmax, ymax - 0.065 * ymax,text_female, verticalalignment='top',
+#             horizontalalignment='right', fontsize=24, fontweight="bold", color="red")
+
+#     plt.plot([xmin+1, xmax- 1], [ymin, ymax], 'k--')
+
+#     plt.xlabel("Post-stroke years", fontsize=30, fontweight="bold")
+#     plt.ylabel(y_label, fontsize=30, fontweight="bold")
+#     plt.title(f"{title} HGS vs Post-stroke years-{target}", fontsize=25, fontweight="bold", y=1.05)
+    
+#     plt.tight_layout()  # Adjust layout to prevent clipping of legend
+
+#     plt.show()
+#     plt.savefig(f"{target}-correlate_{title}_postyears.png")
+#     plt.close()
+
+# ###############################################################################################################################################################
 def create_regplot(target,
-                   x_male,
-                   y_male,
-                   x_female,
-                   y_female,
-                   corr_male,
-                   corr_female,
+                   df,
+                   x,
+                   y,
+                   corr,
                    title,
                    y_label,
 ):
-
+    if x == "years":
+        df["years"] = df["days"]/365
+    y = "hgs_" + y
+    
     fig = plt.figure(figsize=(15,15))
     sns.set_context("poster")
     ax = fig.add_subplot(111)
     plt.tick_params(axis='both', labelsize=30)
-    sns.regplot(x=x_male, y=y_male, line_kws={"color": "grey"}, scatter_kws={"color": "blue"})
-    sns.regplot(x=x_female, y=y_female, scatter_kws={"color": "red"}, line_kws={"color": "grey"})
+    sns.regplot(data=df, x=x, y=y, line_kws={"color": "grey"}, scatter=False)
+    sns.scatterplot(data=df, x=x, y=y, hue="31-0.0", palette=['red', 'blue'])
 
     ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
 
     # Add two text annotations to the corner
-    text_male = 'r= ' + str(format(corr_male, '.3f'))
-    text_female = 'r= ' + str(format(corr_female, '.3f'))
+    text = 'r= ' + str(format(corr, '.3f'))
 
     xmax = np.max(ax.get_xlim())
     ymax = np.max(ax.get_ylim())
     xmin = np.min(ax.get_xlim())
     ymin = np.min(ax.get_ylim())
     
-    plt.text(xmax - 0.15 * xmax, ymax - 0.005 * ymax, text_male, verticalalignment='top',
-            horizontalalignment='right', fontsize=24, fontweight="bold", color="blue")
-    plt.text(xmax - 0.15 * xmax, ymax - 0.065 * ymax,text_female, verticalalignment='top',
-            horizontalalignment='right', fontsize=24, fontweight="bold", color="red")
+    plt.text(xmax - 0.15 * xmax, ymax - 0.005 * ymax, text, verticalalignment='top',
+            horizontalalignment='right', fontsize=24, fontweight="bold", color="black")
 
     plt.plot([xmin+1, xmax- 1], [ymin, ymax], 'k--')
-
+    
+    # Put a legend to the right of the current axis
+    # with setting title
+    legend = ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Gender")
+    # Modify individual legend labels
+    new_legend_labels = ['Female', 'Male']
+    for text, label in zip(legend.get_texts(), new_legend_labels):
+        text.set_text(label)
+    
     plt.xlabel("Post-stroke years", fontsize=30, fontweight="bold")
     plt.ylabel(y_label, fontsize=30, fontweight="bold")
     plt.title(f"{title} HGS vs Post-stroke years-{target}", fontsize=25, fontweight="bold", y=1.05)
@@ -47,5 +100,7 @@ def create_regplot(target,
     plt.tight_layout()  # Adjust layout to prevent clipping of legend
 
     plt.show()
-    plt.savefig(f"{target}-correlate_{title}_postyears.png")
+    plt.savefig(f"both_gender_split_{target}-correlate_{title}_postyears.png")
     plt.close()
+
+###############################################################################################################################################################
