648142bbeb3a4b0ab3d333747fa2b6930f42a662
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2023-08-16 16:23:48 +0200

    Added new codes to make stroke data split to different sub-data based on pre-, post- and logitudinal stroke.

--- exec/2_preprocess_data/1_run_stroke_main_preprocessing.py
+++ exec/2_preprocess_data/1_run_stroke_main_preprocessing.py
@@ -4,6 +4,7 @@ import sys
 
 from hgsprediction.load_data import stroke_load_data
 from hgsprediction.data_preprocessing import stroke_data_preprocessor
+from hgsprediction.save_data import save_preprocessed_data
 
 from ptpython.repl import embed
 # print("===== Done! =====")
@@ -13,10 +14,10 @@ filename = sys.argv[0]
 population = sys.argv[1]
 mri_status = sys.argv[2]
 
+
 df_original = stroke_load_data.load_original_data(population=population, mri_status=mri_status)
+
 ###############################################################################
-print("===== Done! =====")
-embed(globals(), locals())
 
 data_processor = stroke_data_preprocessor.StrokeMainDataPreprocessor(df_original)
 df = data_processor.remove_missing_stroke_dates(df_original)
@@ -25,11 +26,31 @@ df = data_processor.remove_missing_hgs(df)
 
 df = data_processor.define_stroke_type(df)
 df = data_processor.define_followup_days(df)
+
+df_preprocessed = data_processor.preprocess_stroke_df(df)
 ###############################################################################
 df_post_stroke = data_processor.extract_post_stroke_df(df)
 df_pre_stroke = data_processor.extract_pre_stroke_df(df)
-df_longitudinal_sroke = data_processor.extract_longitudinal_stroke_df(df)
+df_longitudinal_stroke = data_processor.extract_longitudinal_stroke_df(df)
+
 ###############################################################################
+
 df_all_post_stroke_visits = data_processor.extract_all_post_stroke_visits(df)
 df_all_pre_stroke_visits = data_processor.extract_all_pre_stroke_visits(df)
+
+###############################################################################
+save_preprocessed_data(df_preprocessed, population, mri_status, stroke_cohort="preprocessed")
+
+save_preprocessed_data(df_post_stroke, population, mri_status, stroke_cohort="post-stroke")
+save_preprocessed_data(df_pre_stroke, population, mri_status, stroke_cohort="pre-stroke")
+save_preprocessed_data(df_longitudinal_stroke, population, mri_status, stroke_cohort="longitudinal-sroke")
+
+save_preprocessed_data(df_all_post_stroke_visits, population, mri_status, stroke_cohort="all_post-stroke")
+save_preprocessed_data(df_all_pre_stroke_visits, population, mri_status, stroke_cohort="all_pre-stroke")
+
+
+print("===== Done! =====")
+embed(globals(), locals())
+
+
 
---
+++ exec/2_preprocess_data/2_run_stroke_hgs_validation.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+import sys
+
+from hgsprediction.load_data import stroke_load_data
+from hgsprediction.data_preprocessing import stroke_data_preprocessor
+
+from ptpython.repl import embed
+# print("===== Done! =====")
+# embed(globals(), locals())
+###############################################################################
+filename = sys.argv[0]
+population = sys.argv[1]
+mri_status = sys.argv[2]
+stroke_cohort = sys.argv[3]
+visit_session = sys.argv[4]
+ 
+df = stroke_load_data.load_preprocessed_data(population, mri_status, stroke_cohort)
+
+###############################################################################
+data_processor = stroke_data_preprocessor.StrokeExtraDataPreprocessor(df, 
+                                                            mri_status,
+                                                            stroke_cohort, 
+                                                            visit_session)
+
+print("===== Done! =====")
+embed(globals(), locals())
+hgs_validated_df = data_processor.validate_handgrips(df)
+
+
+
+
+print("===== Done! =====")
+embed(globals(), locals())
