a882b4f6376456cd7c92479abb6943489e4543a3
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2023-09-06 13:54:59 +0200

    Modified langitudinal stroke hgs boxplots for each target

--- exec/2_preprocess_data/stroke/6_run_stroke_extract_longitudinal_preprocessed_data.py
+++ exec/2_preprocess_data/stroke/6_run_stroke_extract_longitudinal_preprocessed_data.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 from hgsprediction.load_data import stroke_load_data
 from hgsprediction.save_data import stroke_save_data
+from hgsprediction.define_features import define_features
+
 
 from ptpython.repl import embed
 # print("===== Done! =====")
@@ -12,10 +14,13 @@ filename = sys.argv[0]
 population = sys.argv[1]
 mri_status = sys.argv[2]
 stroke_cohort = sys.argv[3]
-visit_session = sys.argv[4]
+feature_type = sys.argv[4]
+visit_session = sys.argv[5]
 
 ###############################################################################
 df_main_preprocessed_longitudinal = stroke_load_data.load_main_preprocessed_data(population, mri_status, stroke_group="only_longitudinal-stroke")
+features = define_features(feature_type)
+
 ###############################################################################
 stroke_cohort = "pre-stroke"
 if visit_session == "1":
@@ -41,6 +46,14 @@ df_post = stroke_load_data.load_preprocessed_data(population, mri_status, sessio
 merged_df = pd.concat([df_pre, df_post], axis=1, join="inner")
 # Select which columns to keep (for example, keep columns from df_pre)
 df = merged_df.loc[:, ~merged_df.columns.duplicated()]
+###############################################################################
+features_columns = [col for col in df.columns if any(col.endswith(item) for item in features)]
+extracted_df1 = df[features_columns].copy()
+extracted_df = extracted_df1.dropna(subset=[col for col in df.columns if any(item in col for item in features)])
+
+df = df[df.index.isin(extracted_df.index)]
+print("===== Done! =====")
+embed(globals(), locals())
 ###############################################################################
 if len(df) > 0:
     df_female = df[df["31-0.0"]==0.0]
--- exec/plots/plot_data_distribution/stroke/hgs_data_distripution_boxplot/boxplots_hgs_pre-post_stroke_distribution.py
+++ exec/plots/plot_data_distribution/stroke/hgs_data_distripution_boxplot/boxplots_hgs_pre-post_stroke_distribution.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import matplotlib.patheffects as path_effects
 from hgsprediction.load_data import stroke_load_data
 
 from ptpython.repl import embed
@@ -14,6 +14,7 @@ filename = sys.argv[0]
 population = sys.argv[1]
 mri_status = sys.argv[2]
 visit_session = sys.argv[3]
+feature_type = sys.argv[4]
 
 stroke_cohort = "pre-post-stroke"
 if visit_session == "1":
@@ -25,9 +26,10 @@ elif visit_session == "3":
 elif visit_session == "4":
     session_column = f"4th_{stroke_cohort}_session"
 
+###############################################################################
 df = stroke_load_data.load_preprocessed_longitudinal_data(population, mri_status, session_column, "both_gender")
 
-
+###############################################################################
 # Assuming you have a DataFrame 'df' with the specified columns
 # Extract the relevant columns
 df_extract_columns_to_plot = df[["31-0.0", "1st_pre-stroke_hgs_left", "1st_post-stroke_hgs_left", "1st_pre-stroke_hgs_right", "1st_post-stroke_hgs_right", "1st_pre-stroke_hgs_L+R", "1st_post-stroke_hgs_L+R"]]
@@ -45,13 +47,13 @@ melted_dfs = []
 # Iterate through column groups and create melted DataFrames
 for group_name, group_columns in column_groups:
     # Melt the DataFrame for the current group
-    melted_group = pd.melt(df_extract_columns_to_plot, id_vars=["31-0.0"], value_vars=group_columns, var_name="variable")
+    melted_group = pd.melt(df_extract_columns_to_plot, id_vars=["31-0.0"], value_vars=group_columns, var_name="variable", ignore_index=False)
     
     # Create 'hgs_category' based on 'group_name'
     melted_group['hgs_category'] = group_name
     
     # Create 'stroke_cohort' based on 'variable'
-    melted_group['stroke_cohort'] = melted_group['variable'].apply(lambda x: 'pre' if 'pre' in x else ('post' if 'post' in x else None))
+    melted_group['stroke_cohort'] = melted_group['variable'].apply(lambda x: 'Pre-stroke' if 'pre' in x else ('Post-stroke' if 'post' in x else None))
     
     # Add a new column based on the combination of 'hgs_category' and 'stroke_cohort'
     melted_group['combine_hgs_stroke_cohort_category'] = group_name + '-' + melted_group['stroke_cohort']
@@ -65,11 +67,28 @@ for group_name, group_columns in column_groups:
     # Append the melted DataFrame to the list
     melted_dfs.append(melted_group)
 
-print(melted_dfs)
+# Concatenate the melted DataFrames into one without ignoring the original indexes
+melted_df = pd.concat(melted_dfs, ignore_index=False)
 
-print("===== Done! =====")
-embed(globals(), locals())
-
+print(melted_df)
+###############################################################################
+def add_median_labels(ax, fmt='.1f'):
+    lines = ax.get_lines()
+    boxes = [c for c in ax.get_children() if type(c).__name__ == 'PathPatch']
+    lines_per_box = int(len(lines) / len(boxes))
+    for median in lines[4:len(lines):lines_per_box]:
+        x, y = (data.mean() for data in median.get_data())
+        # choose value depending on horizontal or vertical plot orientation
+        value = x if (median.get_xdata()[1] - median.get_xdata()[0]) == 0 else y
+        text = ax.text(x, y, f'{value:{fmt}}', ha='center', va='center',  color='white', fontsize=10)
+                    #    fontweight='bold',
+        # create median-colored border around white text for contrast
+        text.set_path_effects([
+            path_effects.Stroke(linewidth=3, foreground=median.get_color()),
+            path_effects.Normal(),
+        ])
+        
+###############################################################################
 # Define a custom palette with two blue colors
 custom_palette = sns.color_palette(['#95CADB', '#008ECC'])  # You can use any hex color codes you prefer
 
@@ -77,61 +96,40 @@ custom_palette = sns.color_palette(['#95CADB', '#008ECC'])  # You can use any he
 plt.figure(figsize=(10, 6))  # Adjust the figure size if needed
 sns.set(style="whitegrid")
 # sns.set_context("poster", font_scale=1.25)
-ax = sns.boxplot(x="hgs_category", y="value", hue="stroke_cohort", data=final_df, palette=custom_palette)
+ax = sns.boxplot(x="hgs_category", y="value", hue="stroke_cohort", data=melted_df, palette=custom_palette)
 
 # Add labels and title
 plt.xlabel("HGS targets", fontsize=20, fontweight="bold")
 plt.ylabel("HGS values", fontsize=20, fontweight="bold")
-plt.title("pre-stroke and post-stroke HGS values", fontsize=20, fontweight="bold")
+plt.title(f"pre-stroke and post-stroke HGS values - {feature_type}", fontsize=20)
 
 ymin , ymax = ax.get_ylim()
 ystep = 10
 #set y-axis ticks (step size=5)
-plt.yticks(np.arange(0, 120, 5))
+plt.yticks(np.arange(0, 120, 10))
 # Show the plot
 plt.legend(title="stroke cohort", loc="upper left")  # Add legend
+legend = plt.legend(title="Gender", loc="upper left")  # Add legend
+# Modify individual legend labels
+legend.get_texts()[0].set_text(f"Pre-stroke: N={len(df)}")
+legend.get_texts()[1].set_text(f"Post-stroke: N={len(df)}")
+
 plt.tight_layout()
+
+add_median_labels(ax)
+
 plt.show()
 plt.savefig("hh.png")
-
-
-print("===== Done! =====")
-embed(globals(), locals())
-
-# Assuming you have a DataFrame 'df' with the specified columns
-# Extract the relevant columns
-df1 = df[["31-0.0", "1st_pre-stroke_hgs_left", "1st_post-stroke_hgs_left", "1st_pre-stroke_hgs_right", "1st_post-stroke_hgs_right", "1st_pre-stroke_hgs_L+R", "1st_post-stroke_hgs_L+R"]]
-
-# Melt the DataFrame with the specified categories
-melted_df = pd.melt(df1, id_vars=["31-0.0"], var_name="variable")
-
-# Create 'hgs_category' based on 'variable'
-melted_df['hgs_category'] = melted_df['variable'].apply(lambda x: 'left' if 'left' in x else ('right' if 'right' in x else 'L+R'))
-
-# Create 'stroke_cohort' based on 'variable'
-melted_df['stroke_cohort'] = melted_df['variable'].apply(lambda x: 'pre' if 'pre' in x else ('post' if 'post' in x else None))
-
-# Assuming you have a DataFrame 'melted_df' with 'hgs_category' and 'stroke_cohort' columns
-# Add a new column based on the combination of 'hgs_category' and 'stroke_cohort'
-melted_df['hgs_stroke_cohort_category'] = melted_df['hgs_category'] + '-' + melted_df['stroke_cohort']
-
-# Create 'gender' based on 'gender' column
-melted_df['gender'] = melted_df['31-0.0'].map({0: 'female', 1: 'male'})
-
-# Drop the original 'variable' column
-melted_df.drop(columns=['31-0.0'], inplace=True)
-
-# Print the melted DataFrame
-print(melted_df)
-
+plt.close()
+###############################################################################
 # Define a custom palette with two blue colors
 custom_palette = sns.color_palette(['#29AB87', '#BC544B'])  # You can use any hex color codes you prefer
 # Create the boxplot for 'hgs_category' and 'gender'
 plt.figure(figsize=(12, 6))
 sns.set(style="whitegrid")
-sns.boxplot(x="hgs_stroke_cohort_category", y="value", data=melted_df, hue="gender", palette=custom_palette)
+ax = sns.boxplot(x="combine_hgs_stroke_cohort_category", y="value", data=melted_df, hue="gender", palette=custom_palette)
 
-plt.yticks(np.arange(0, 120, 5))
+plt.yticks(np.arange(0, 120, 10))
 # # Extract unique values of 'hgs_category' and 'stroke_cohort'
 unique_hgs_categories = melted_df["hgs_category"].unique()
 unique_stroke_cohorts = melted_df["stroke_cohort"].unique()
@@ -141,15 +139,27 @@ xticks_labels = [f"{cohort}" for hgs_category in unique_hgs_categories for cohor
 # Set xtick positions and labels
 plt.xticks(range(len(xticks_labels)), xticks_labels)
 
-
 # Add labels and title
 plt.xlabel("HGS targets", fontsize=20, fontweight="bold")
 plt.ylabel("HGs values", fontsize=20, fontweight="bold")
-plt.title("HGS values for genders", fontsize=20, fontweight="bold")
-plt.legend(title="Gender", loc="upper left")  # Add legend
+plt.title(f"HGS values for genders - {feature_type}", fontsize=20)
+legend = plt.legend(title="Gender", loc="upper left")  # Add legend
+# Modify individual legend labels
+legend.get_texts()[0].set_text(f"Female: N={len(df[df['31-0.0']==0.0])}")
+legend.get_texts()[1].set_text(f"Male: N={len(df[df['31-0.0']==1.0])}")
 
 # Show the plot
 plt.tight_layout()
+
+add_median_labels(ax)
+
 plt.show()
 plt.savefig("hhfm.png")
+plt.close()
+
+
+
+print("===== Done! =====")
+embed(globals(), locals())
+
 
