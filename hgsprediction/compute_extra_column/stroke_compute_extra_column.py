5cf0eddca050ef0f2a0c3fef23ee23f4287ab658
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2023-10-31 11:35:13 +0100

    Added check mri data in ukb and plots for parkinson

--- hgsprediction/compute_extra_column/stroke_compute_extra_column.py
+++ hgsprediction/compute_extra_column/stroke_compute_extra_column.py
@@ -25,40 +25,61 @@ def compute_extra_column(df, session_column, extra_column):
         df = calculate_cutoff_value_hgs(df, session_column)     
             
 ###############################################################################
-def calculate_cutoff_value_hgs(df, session_column):
-
+def calculate_age_range(df, session):
     # Define your age bins/ranges
     bins = np.arange(35, 80, 5)
     labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]  # Adjusted labels
 
     # Use pd.cut() to create the age range column
     df["age_range"] = pd.cut(df["age"], bins=bins, labels=labels, right=False)  # Adjusted labels and right parameter
+    
+    return df
+###############################################################################
+def calculate_cutoff_value_hgs(df, session):
+    # Define age bins and labels
+    bins = np.arange(35, 80, 5)
+    labels = [f"{bins[i]}-{bins[i+1]-1}" for i in range(len(bins)-1)]
 
-    # Define age-specific percentiles and cutoff values
+    df["age_range"] = pd.cut(df["age"], bins=bins, labels=labels, right=False)  # Adjusted labels and right parameter
+
+    # Create a dictionary to store cutoff DataFrames for each gender
+    cutoff_dfs = {}
+
+    # Define age-specific percentiles
     percentiles = [10, 20, 25, 30, 40, 50, 75, 90, 95]
 
-    # Initialize a dictionary to store cutoff values for each age range and each specified percentile
-    cutoff_values = {"age_range": labels}
-    for p in percentiles:
-        cutoff_values[f"{p}th Percentile"] = []
-
-    # Calculate cutoff values for each age range and each specified percentile
-    for age_range in labels:
+    # Calculate cutoff values for each gender
+    for gender in df["gender"].unique():
+        # Initialize a dictionary to store cutoff values for this gender and age range
+        cutoff_values = {"age_range": labels}
         for p in percentiles:
-            age_group_data = df[(df["age_range"] == age_range)]["hgs_dominant-0.0"]
-            if not age_group_data.empty:
-                cutoff_value = np.percentile(age_group_data, p)
-                cutoff_values[f"{p}th Percentile"].append(cutoff_value)
-            else:
-                cutoff_values[f"{p}th Percentile"].append(None)
+            cutoff_values[f"{p}th_percentile"] = []
 
-    # Create a DataFrame from the cutoff values dictionary
-    cutoff_df = pd.DataFrame(cutoff_values)
+        # Filter the DataFrame by gender
+        df_gender = df[df["gender"] == gender]
 
-    # Merge the cutoff values back into the original DataFrame
-    df = df.merge(cutoff_df, on="age_range", how="left")
+        # Calculate cutoff values for each age range and each specified percentile for this gender
+        for age_range in labels:
+            for p in percentiles:
+                age_group_data = df_gender[df_gender["age_range"] == age_range][f"hgs_dominant-{session}.0"]
+                if not age_group_data.empty:
+                    cutoff_value = np.percentile(age_group_data, p)
+                    cutoff_values[f"{p}th_percentile"].append(cutoff_value)
+                else:
+                    cutoff_values[f"{p}th_percentile"].append(None)
 
-    # Now, df contains the original data with age ranges and cutoff percentiles
+        # Create a DataFrame for this gender and store it in the dictionary
+        cutoff_df = pd.DataFrame(cutoff_values)
+        cutoff_dfs[gender] = cutoff_df
+    # Merge and concatenate DataFrames by gender while preserving the original index
+    df_merged_female = df[df["gender"] == 0].merge(cutoff_dfs[0], on="age_range", how="left")
+    df_merged_male = df[df["gender"] == 1].merge(cutoff_dfs[1], on="age_range", how="left")
 
-    return df            
+    # Concatenate the two DataFrames
+    result_df = pd.concat([df_merged_female, df_merged_male])
 
+    # Reset the index to match the original df index
+    result_df.set_index(df.index, inplace=True)
+
+    return result_df
+###############################################################################
