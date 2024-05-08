55ba9893e8a87bb8f1d6c7b2e44f5dce227608cb
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2024-05-08 15:08:59 +0200

    Modified codes for hgs vs predicted

--- hgsprediction/predict_hgs/predict_hgs.py
+++ hgsprediction/predict_hgs/predict_hgs.py
@@ -8,12 +8,10 @@ from ptpython.repl import embed
 
 def predict_hgs(df, X, y, best_model_trained, target):
     
-    # y_True
-    # df.loc[:, f"{target}_true"] = df.loc[:, y].copy()
+    # y_True --> df.loc[:, f"{target}"]
     # y_predicted
     df.loc[:, f"{target}_predicted"] = best_model_trained.predict(df.loc[:, X])
     # error: (true-predicted)
-    # df.loc[:, f"{target}_delta(true-predicted)"] =  df.loc[:, f"{target}_predicted"] - df.loc[:, f"{target}"]
     df.loc[:, f"{target}_delta(true-predicted)"] =  df.loc[:, f"{target}"] - df.loc[:, f"{target}_predicted"]
 
     return df
