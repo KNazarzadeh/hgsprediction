be040e177e0c61fb88dabbb906a2b39487e6a60e
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2024-05-09 22:40:23 +0200

    Modified codes for hgs vs predicted

---
+++ exec/9_test_trained_models_on_diseases/8_load_ANOVA_results.py
@@ -0,0 +1,58 @@
+import math
+import sys
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from statsmodels.stats.anova import AnovaRM
+
+from hgsprediction.load_results.load_prepared_data_for_anova import load_prepare_data_for_anova
+from hgsprediction.save_results.save_anova_results import save_anova_results
+from pingouin import mixed_anova
+import statsmodels.stats.multicomp as multi
+import statsmodels.stats.multicomp as mc
+
+from ptpython.repl import embed
+# print("===== Done! =====")
+# embed(globals(), locals())
+
+###############################################################################
+filename = sys.argv[0]
+population = sys.argv[1]
+mri_status = sys.argv[2]
+feature_type = sys.argv[3]
+model_name = sys.argv[4]
+confound_status = sys.argv[5]
+n_repeats = sys.argv[6]
+n_folds = sys.argv[7]
+disorder_cohort = sys.argv[8]
+visit_session = sys.argv[9]
+n_samples = sys.argv[10]
+target = sys.argv[11]
+anova_target = sys.argv[12]
+##############################################################################
+disorder_cohort = f"{disorder_cohort}-{population}"
+if visit_session == "1":
+    session_column = f"1st_{disorder_cohort}_session"
+##############################################################################
+
+df = load_anova_results(
+        mixedlm_model_fit,
+        population,
+        mri_status,
+        session_column,
+        model_name,
+        feature_type,
+        target,
+        confound_status,
+        n_repeats,
+        n_folds,
+        n_samples,
+        anova_target,
+        "non",
+        "mixedlm_both_gender",
+    )
