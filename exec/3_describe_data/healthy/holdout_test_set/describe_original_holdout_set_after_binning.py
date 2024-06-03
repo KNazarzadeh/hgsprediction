cc02e3499e57ab518016d2b16e8996185b894180
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2024-06-03 15:35:50 +0200

    ANOVA analysis

--- exec/3_describe_data/healthy/holdout_test_set/describe_original_holdout_set_after_binning.py
+++ exec/3_describe_data/healthy/holdout_test_set/describe_original_holdout_set_after_binning.py
@@ -12,27 +12,29 @@ from ptpython.repl import embed
 filename = sys.argv[0]
 population = sys.argv[1]
 mri_status = sys.argv[2]
-data_set = sys.argv[3]
+session = sys.argv[3]
+data_set = sys.argv[4]
 ###############################################################################
 # load data # Extract data based on main features, extra features, target for each session and mri status:
-df = load_healthy_data.load_original_data(population, mri_status)
+df = load_healthy_data.load_original_nonmri_test_data(population, mri_status)
 
 df_female = df[df['31-0.0'] == 0]
 df_male = df[df['31-0.0'] == 1]
 ###############################################################################
 whole_data_length = len(df_female)+len(df_male)
-print("\n Number of whole Original MRI data N=", whole_data_length)
+print("\n Number of whole Original Training data N=", whole_data_length)
 
-print("\n Number of Female Original MRI data N=", len(df_female))
-print("'%' of Female Original MRIt data N=%", "{:.2f}".format(len(df_female)*100/whole_data_length))
+print("\n Number of Female Original Training data N=", len(df_female))
+print("'%' of Female Original Trainingt data N=%", "{:.2f}".format(len(df_female)*100/whole_data_length))
 
-print("\n Number of Male Original MRI data N=", len(df_male))
-print("'%' of Male Original MRI data N=%", "{:.2f}".format(len(df_male)*100/whole_data_length))
+print("\n Number of Male Original Training data N=", len(df_male))
+print("'%' of Male Original Training data N=%", "{:.2f}".format(len(df_male)*100/whole_data_length))
 
-print("\n Female Describe=\n", df_female.describe().apply(lambda x: round(x, 2)))
+female_describe = df_female.describe().apply(lambda x: round(x, 2))
+print("\n Female Describe=\n", female_describe)
 
-print("\n Male Describe=\n", df_male.describe().apply(lambda x: round(x, 2)))
-
+male_describe = df_male.describe().apply(lambda x: round(x, 2))
+print("\n Male Describe=\n", male_describe)
 
 print("===== Done! =====")
 embed(globals(), locals())
