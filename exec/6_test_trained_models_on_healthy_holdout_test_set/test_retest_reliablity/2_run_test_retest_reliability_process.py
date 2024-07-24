e403ce8115abaef01c5245ae12e33e51533c3c87
KNazarzadeh <kimia.nazarzadeh@gmail.com>
2024-07-24 09:38:13 +0200

    Preprocess test dataset for first-repeat assessment visit

--- exec/6_test_trained_models_on_healthy_holdout_test_set/test_retest_reliablity/2_run_test_retest_reliability_process.py
+++ exec/6_test_trained_models_on_healthy_holdout_test_set/test_retest_reliablity/2_run_test_retest_reliability_process.py
@@ -93,54 +93,62 @@ def concordance_correlation_coefficient(test_session_0, retest_session_1):
     return ccc
 
 ###############################################################################
+print("\ngender: ", gender)
+print("----------------------------------------------------------")
 # Calculate CCC between corrected delta hgs and raw hgs for each session 0
 test_session_0 = df_session_0[f"{target}_predicted"]
 retest_session_1 = df_session_1[f"{target}_predicted"]
 
 ccc_value = concordance_correlation_coefficient(test_session_0, retest_session_1)
-print(f"Concordance Correlation Coefficient: {ccc_value:.2f}")
+print(f"Concordance Correlation Coefficient for predicted values without bias-adjustment: {ccc_value:.2f}")
 ###############################################################################
 # Calculate CCC between corrected delta hgs and raw hgs for each session 0
 test_session_0 = df_session_0[f"{target}_corrected_predicted"]
 retest_session_1 = df_session_1[f"{target}_corrected_predicted"]
 
 ccc_value = concordance_correlation_coefficient(test_session_0, retest_session_1)
-print(f"Concordance Correlation Coefficient: {ccc_value:.2f}")
+print(f"Concordance Correlation Coefficient for predicted values with bias-adjustment: {ccc_value:.2f}")
 ###############################################################################
 # Calculate CCC between corrected delta hgs and raw hgs for each session 0
 test_session_0 = df_session_0[f"{target}_delta(true-predicted)"]
 retest_session_1 = df_session_1[f"{target}_delta(true-predicted)"]
 
 ccc_value = concordance_correlation_coefficient(test_session_0, retest_session_1)
-print(f"Concordance Correlation Coefficient: {ccc_value:.2f}")
+print(f"\nConcordance Correlation Coefficient for delta values without bias-adjustment: {ccc_value:.2f}")
 ###############################################################################
 # Calculate CCC between corrected delta hgs and raw hgs for each session 0
 test_session_0 = df_session_0[f"{target}_corrected_delta(true-predicted)"]
 retest_session_1 = df_session_1[f"{target}_corrected_delta(true-predicted)"]
 
 ccc_value = concordance_correlation_coefficient(test_session_0, retest_session_1)
-print(f"Concordance Correlation Coefficient: {ccc_value:.2f}")
+print(f"Concordance Correlation Coefficient for delta values with bias-adjustment: {ccc_value:.2f}")
 ###############################################################################
 age_differ = df_session_1.loc[:, "age"] - df_session_0.loc[:, "age"]
 min_age = min(age_differ)
 max_age = max(age_differ)
 
 ###############################################################################
+print("----------------------------------------------------------")
+
 y_true_session_0 = df_session_0[f"{target}"]
 y_pred_session_0 = df_session_0[f"{target}_predicted"]
 mae_ses_0_predicted = mean_absolute_error(y_true_session_0, y_pred_session_0)
+print(f"MAE for predicted values of session 0 without bias-adjustment: {mae_ses_0_predicted:.3f}")
 
 y_true_session_1 = df_session_1[f"{target}"]
 y_pred_session_1 = df_session_1[f"{target}_predicted"]
 mae_ses_1_predicted = mean_absolute_error(y_true_session_1, y_pred_session_1)
+print(f"MAE for predicted values of session 1 without bias-adjustment: {mae_ses_1_predicted:.3f}")
 
 y_true_session_0 = df_session_0[f"{target}"]
 y_pred_session_0 = df_session_0[f"{target}_corrected_predicted"]
 mae_ses_0_corrected_predicted = mean_absolute_error(y_true_session_0, y_pred_session_0)
+print(f"\nMAE for predicted values of session 0 with bias-adjustment: {mae_ses_0_corrected_predicted:.3f}")
 
 y_true_session_1 = df_session_1[f"{target}"]
 y_pred_session_1 = df_session_1[f"{target}_corrected_predicted"]
 mae_ses_1_corrected_predicted = mean_absolute_error(y_true_session_1, y_pred_session_1)
+print(f"MAE for predicted values of session 1 with bias-adjustment: {mae_ses_1_corrected_predicted:.3f}")
 
 print("===== Done! =====")
 embed(globals(), locals())
